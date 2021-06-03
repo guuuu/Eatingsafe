92ff07499b84298e6d0c02d37266d6bd0553e9bd
Gustavo Nascimento <guganascimento2000@gmail.com>
2021-06-03 04:51:35 +0100

    Register done | started profile & sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//login
 $(".field_input").focus(function() { $($(this).parent()[0]).css("border-bottom", "2px solid #f8c365"); });
 $(".field_input").focusout(function() { $($(this).parent()[0]).css("border-bottom", "2px solid #C77136"); });
 
@@ -5,13 +6,99 @@ $("#bt").on("click", () => {
     $.ajax({
         url: "/do_login",
         method: "POST",
-        data: {
-            user: $($(".field_input")[0]).val(),
-            pwd: $($(".field_input")[1]).val()
-        }
+        data: { user: $($(".field_input")[0]).val(), pwd: $($(".field_input")[1]).val() }
     }).done((response) => {
-        console.log(response);
-        $($(".field_input")[0]).val("");              
-        $($(".field_input")[1]).val("");
+        if(response["status"] === 0){
+            window.location.href = "profile"
+        }
+        else{
+            alert("Login mal")
+        }
+        // console.log(response);
+        // $($(".field_input")[0]).val("");              
+        // $($(".field_input")[1]).val("");
     });
 });
+//login
+
+//registar
+$(".check").click(function(){
+    $(".check").each(function() { if(!$($(this).children()[0]).hasClass("hide")) { $($(this).children()[0]).addClass("hide"); } });
+    $($(this).children()[0]).removeClass("hide");
+});
+
+$(".drop_arrow").click(function() {
+    //$($($(this).parent()[0]).parent()).children()[0]
+    console.log($($($(this).parent()[0]).parent()).children()[0]);
+});
+
+function add_options(){
+
+}
+
+$(".r").click(() => {
+    let div = document.createElement("div");
+    let select = document.createElement("select");
+    let div2 = document.createElement("div");
+    let img = document.createElement("img");
+
+    $(select).attr("name", "alergies");
+    $(select).attr("id", "3");
+    $(select).attr("class", "drop");
+
+    $(div).attr("class", "alergie");
+    $(div2).attr("class", "wrap");
+
+    $(img).attr("src", "assets/icons/chevron-pointing-to-the-left.png");
+    $(img).attr("alt", "arrow");
+    $(img).attr("class", "drop_arrow");
+
+    div2.appendChild(img);
+    div.appendChild(select);
+    div.appendChild(div2);
+
+    $(".alergies")[0].append(div);
+})
+
+$(".bt_register").click(() => {
+    if($($(".field_input")[2]).val() == $($(".field_input")[3]).val()){
+        //console.log($($(".field_input")[2]).val());
+        let gender = "";
+        let alergies = new Array();
+
+        $(".check").each(function() {
+            if(!$($(this).children()).hasClass("hide")){
+                gender = String($($($(this).parent()).children()[0]).text()).substr(0, 1) 
+            } 
+        });
+
+        $(".alergie").each(function() {
+            let value = parseInt($($($(this).children()[0]).children("option:selected")).val());
+            if(!alergies.includes(value) && value != -1 ){
+                alergies.push(value)
+            }
+        });
+
+        let data = {
+            user: $($(".field_input")[0]).val(),
+            email: $($(".field_input")[1]).val(),
+            pwd: $($(".field_input")[2]).val(),
+            age: $($(".field_input")[3]).val(),
+            gender: gender,
+            alergies: alergies
+        }
+
+        $.ajax({
+            url: "/do_regist",
+            method: "POST",
+            dataType: "JSON",
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done((response) => {
+            console.log(response);
+        })
+    }
+    else{
+        console.log("Passwords diferentes");
+    }
+})
--- assets/js/server.js
+++ assets/js/server.js
@@ -12,10 +12,36 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname + "\\..\\..", "login.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname + "\\..\\..", "register.html"));
+});
+
+app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname + "\\..\\..", "profile.html"));
+});
+
 app.post("/do_login", (req, res) => {
     const { user, pwd } = req.body;
-    res.status(200).send({user: user, pwd: pwd});
+    if(user === "admin" && pwd === "admin"){
+        res.status(200).send({status: 0});
+    }
+    else{
+        res.status(200).send({status: 1});
+    }
 });
+
+app.post("/do_regist", (req, res) => {
+    const { user, email, pwd, age, gender, alergies } = req.body;
+
+    res.status(200).send({
+        user: user,
+        email: email,
+        pwd: pwd,
+        age: age,
+        gender: gender,
+        alergies: alergies
+    });
+})
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname + "\\..\\..", "error.html"));
